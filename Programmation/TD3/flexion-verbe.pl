c33039a5515184042426f8ac9de6f429d352b635
MaximeD <akbarova.armia@gmail.com>
2011-10-01 10:43:52 +0300

    playing with subroutines

--- Programmation/TD3/flexion-verbe.pl
+++ Programmation/TD3/flexion-verbe.pl
@@ -2,33 +2,35 @@
 use strict ;
 
 my ( $radical, $inf, $verb, $i ) ;
+my @terminaisons ;
 my @pronoms = ( "je", "tu", "il/elle", "nous", "vous", "ils/elles" ) ;
 my @terminaisons_1 = ( "e", "es", "e", "ons", "ez", "ent" ) ;
 my @terminaisons_2 = ( "is", "is", "it", "issons", "issez", "issent" ) ;
 
-print "Entrez un verbe du 1er ou du 2eme groupe : " ;
-chomp($verb=<STDIN>) ;
-if ($verb =~ m/er$/){
-    $inf = $verb ;
-    $inf =~ s/er$//;
-        
+sub Conjugate{
     print "Les formes du verbe \"$verb\" au présent de l'indicatif sont :\n" ;
     
     for ($i=0; $i<@pronoms; $i++)
     {
-	print $pronoms[$i] . " " . $inf . $terminaisons_1[$i] . "\n" ;
-    }
-}
-elsif ($verb =~ m/ir$/){
-    $inf = $verb ;
-    $inf =~ s/ir$//;
-        
-    print "Les formes du verbe \"$verb\" au présent de l'indicatif sont :\n" ;
-    
-    for ($i=0; $i<@pronoms; $i++)
-    {
-	print $pronoms[$i] . " " . $inf . $terminaisons_2[$i] . "\n" ;
+	print $pronoms[$i] . " " . $inf . $terminaisons[$i] . "\n" ;
     }
 }
 
-else{print "\"$verb\" n'est pas un verbe du premier ou du secpnd groupe !\n"}
+print "Entrez un verbe du 1er ou du 2eme groupe : " ;
+chomp($verb=<STDIN>) ;
+
+if ($verb =~ m/er$/){
+    $inf = $verb ;
+    $inf =~ s/er$//;
+    @terminaisons = @terminaisons_1 ;
+    &Conjugate ;
+}       
+ 
+elsif ($verb =~ m/ir$/){
+    $inf = $verb ;
+    $inf =~ s/ir$//;
+    @terminaisons = @terminaisons_2 ;
+    &Conjugate ;
+}
+
+else{print "\"$verb\" n'est pas un verbe du premier ou du second groupe !\n"}

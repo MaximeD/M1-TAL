094c1bd654d0f36cd20d632ba8bc73c9ba2f54d5
Maxime Demolin <akbarova.armia@gmail.com>
2011-10-07 22:27:26 +0200

    got the point

--- Programmation/TD3/ex5.rb
+++ Programmation/TD3/ex5.rb
@@ -4,8 +4,8 @@ array, places =[1,2,3,8,5,8,0,4], []
 
 puts "Maximum value of array is " + array.max.to_s
 
-array.each_index do |x|
-    places << x if array[x] == array.max
+array.each_with_index do |value,index|
+    places << index if value == array.max
 end
 
 puts "You can find " + array.max.to_s + " in position " + places.join(", ")

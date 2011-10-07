8baf478f593574b0d7e698174fd7bdc5ee5f02bf
Maxime Demolin <akbarova.armia@gmail.com>
2011-10-07 20:45:56 +0200

    gotta catch'em all

--- Programmation/TD3/ex5.rb
+++ Programmation/TD3/ex5.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/ruby
 
 array=[1,2,3,8,5,8,0,4]
+places = Array.new
 
 puts "Maximum value of array is " + array.max.to_s
-puts "You can find " + array.max.to_s + " in position " + array.index(array.max).to_s
 
+array.each_index{|x|
+  if (array[x] == array.max)
+    places << x
+  end
+}
+
+puts "You can find " + array.max.to_s + " in position " + places.join(", ")

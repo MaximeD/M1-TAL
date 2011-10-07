f0f13707e637db026094813bbbecec776aedb796
Maxime Demolin <akbarova.armia@gmail.com>
2011-10-07 22:00:05 +0200

    merge and SIMPLIFY oelmekki version

--- Programmation/TD3/ex5.rb
+++ Programmation/TD3/ex5.rb
@@ -1,14 +1,11 @@
 #!/usr/bin/ruby
 
-array=[1,2,3,8,5,8,0,4]
-places = Array.new
+array, places =[1,2,3,8,5,8,0,4], []
 
 puts "Maximum value of array is " + array.max.to_s
 
-array.each_index{|x|
-  if (array[x] == array.max)
-    places << x
-  end
-}
+array.each_index do |x|
+    places << x if array[x] == array.max
+end
 
 puts "You can find " + array.max.to_s + " in position " + places.join(", ")

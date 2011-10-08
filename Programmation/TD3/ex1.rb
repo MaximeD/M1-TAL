713204b228ab491b7a379911a488a3ff0fa9bd94
Maxime Demolin <akbarova.armia@gmail.com>
2011-10-08 16:17:21 +0200

    rm useless lines

--- Programmation/TD3/ex1.rb
+++ Programmation/TD3/ex1.rb
@@ -1,17 +1,12 @@
 #!/usr/bin/ruby
 
-puts  "Entrez un nom commun singulier"
+puts  "Entrez un nom commun singulier :"
 nom = gets.chomp
-
-pluriel = nom + "s"
 
 invar = ["ours", "lynx", "putois"]
 
-if invar.include?(nom)
-  pluriel = nom
-else
-  pluriel = nom + "s"
-end
+pluriel = nom + "s"
+pluriel = nom if invar.include?(nom)
 
-puts "Les flexions de " + nom + " sont :\n"
+puts "\nLes flexions de " + nom + " sont :\n"
 puts "un " + nom + ", des " + pluriel

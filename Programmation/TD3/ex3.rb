479741d1b450cea1f4f0612fd9260e2571540778
Maxime Demolin <akbarova.armia@gmail.com>
2011-10-08 16:21:02 +0200

    var in the quotes

--- Programmation/TD3/ex3.rb
+++ Programmation/TD3/ex3.rb
@@ -5,7 +5,7 @@ array=[1,2,3,5,0,4]
 value_to_search = 8
 
 if array.include?(value_to_search)
-  puts value_to_search.to_s + " is in the array !\n"
+  puts "#{value_to_search} is in the array !\n"
 else
-  puts value_to_search.to_s + " is not in the array !\n"
+  puts "#{value_to_search} is not in the array !\n"
 end

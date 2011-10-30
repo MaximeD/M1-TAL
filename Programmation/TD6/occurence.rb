a344b47c1a4a2fc9bc8d5dd2b7dd13c6a18f0102
Maxime Demolin <akbarova.armia@gmail.com>
2011-10-30 20:35:36 +0100

    first commit

---
+++ Programmation/TD6/occurence.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+text = String.new
+File.open('conteGrimm.txt', 'r') { |line| text = line.read}
+words = text.split(/[ \n\.\?'\:,‒;]/)
+
+occurence = Hash.new(0)
+words.each { |word| occurence[word] += 1 unless word == ""}
+
+puts "The 10 most frequent words are :\n"
+occurence.sort_by { |k,v| v}.reverse.first(10).each {|word, freq| puts word + ' : ' + freq.to_s}
+
+
+puts "\nThere is just one occurence of the following terms :\n"
+once = Array.new
+occurence.each { |word, freq| once << word if freq == 1}
+puts once.join(", ")
+
+
+puts "\nAnd last (but not least!), the 10 longest words are :\n"
+long = Array.new
+occurence.first(10).sort_by { |k,v| k.length}.reverse.each{ |word,freq| long << word}
+puts long.join(", ")

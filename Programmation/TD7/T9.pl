980a3b8bc05660be2c5e698289c8e7ab08c47b16
Maxime Demolin <akbarova.armia@gmail.com>
2011-11-07 19:36:38 +0100

    regex builder + no var for split

--- Programmation/TD7/T9.pl
+++ Programmation/TD7/T9.pl
@@ -8,15 +8,14 @@ use Encode qw(encode decode);
 # Simulates cellphone's T9
 
 my %t9 = (
-	       1 => "",
-	       2 => "[abcàâç]",
-	       3 => "[deféèê]",
-	       4 => "[ghi]",
-	       5 => "[jkl]" ,
-	       6 => "[mnoô]" ,
-	       7 => "[pqrs]" ,
-	       8 => "[tuvù]" ,
-	       9 => "[wxyz]"
+	       2 => "abcàâç",
+	       3 => "deféèê",
+	       4 => "ghi",
+	       5 => "jkl" ,
+	       6 => "mnoô" ,
+	       7 => "pqrs" ,
+	       8 => "tuvù" ,
+	       9 => "wxyz"
 	      ) ;
 
 open(LEXIQUE, '<:utf8', "lexique.txt") or die ;
@@ -24,28 +23,28 @@ open(LEXIQUE, '<:utf8', "lexique.txt") or die ;
 print encode("utf8","Veuillez entrer une séquence de touches\n") ;
 chomp(my $touches = <STDIN>) ;
 die encode("utf8","Veuillez n'entrer que des chiffres") if $touches =~ /\D/ ;
-
-my @touche = split(//, $touches) ; # isolate digits
+die encode("utf8","Le chiffre 1 n'est associé à aucune lettre !") if $touches =~ /1+/ ;
 
 # T9 convertion :
-my $sequence ;
-foreach my $i(@touche) {
-  $sequence .= $t9{$i} ;
+
+my $re ;
+foreach my $i(split(//, $touches)) { # split: isolate digits and get array
+  $re .= "[" . $t9{$i} . "]" ;
 }
 
-chomp(my @possibles = grep(/\b$sequence\b/, <LEXIQUE>)) ;
+chomp(my @possibles = grep(/^$re$/, <LEXIQUE>)) ;
 
 if (!@possibles) {
   print encode("utf8","Aucun mot ne correspond\n") ;
 }
-elsif (@possibles == 1) {
-  print encode("utf8","La seule possibilité trouvée est :\n");
-  print encode("utf8","\"$possibles[0]\"\n") ;
-}
 else {
-  print encode("utf8","Les possibilités sont les suivantes :\n");
+    if (@possibles == 1) {
+	print encode("utf8","La seule possibilité trouvée est :\n");
+    }
+    else {
+	print encode("utf8","Les possibilités sont les suivantes :\n");
+    }
   print encode("utf8","\"" . join("\", \"", @possibles) . "\"\n") ;
 }
 
 close(LEXIQUE) ;
-

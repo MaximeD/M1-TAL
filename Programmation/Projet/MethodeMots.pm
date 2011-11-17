b974096d1add315825886fb6315ac44b8c428a24
Maxime Demolin <akbarova.armia@gmail.com>
2011-11-17 19:20:43 +0100

    afternoon work

--- Programmation/Projet/MethodeMots.pm
+++ Programmation/Projet/MethodeMots.pm
@@ -21,68 +21,92 @@ sub poids {
 
   my %freq ;
   my $count_words = 0 ;                 # total words in file / reinitialize
-
+  
   open($_[0], '<:utf8', $_[1]);
-
+  
   my $fileHandle = $_[0];               # a way to pass a filehandle as an argument
   while (<$fileHandle>) {
-    chomp($_); # rm newlines
-    my @mots = split(/\pP|\pS|\s/, $_); # extract words
-    foreach my $mot(@mots) {
-      if ($mot ne ""){
-	$count_words++ ;
-	$freq{ lc ($mot) }++ ;
+      chomp($_);                          # rm newlines
+      my @mots = split(/\pP|\pS|\s/, $_); # extract words
+      foreach my $mot(@mots) {
+	  if ($mot ne ""){
+	      $count_words++ ;
+	      $freq{ lc ($mot) }++ ;
+	  }
       }
-    }
   }
-
+  
   # sorting hash
   my @sorted = sort { ( $freq{$b} <=> $freq{$a}) or ($a cmp $b) } keys %freq ;
-
+  
   # most frequent :
-  print "\n\#\#\#\#\#\#\#\#\#\# OUTPUT Test \#\#\#\#\#\#\#\#\#\#\n";
-  print "Les mots les plus fréquents de $_[1] et leurs poids sont :\n";
   my $i ;
+  my %hash;
+  print "\#\#\#\#\#\#\#\#\#\#\#\#\#\n" ;
   for ($i = 0 ; $i < 10 ; $i++) {
-    print "$sorted[$i] \t". $freq{$sorted[$i]}/$count_words*100 . "\n";
+      $hash{$sorted[$i]} = $freq{$sorted[$i]}/$count_words*100 ;
+      print "$sorted[$i] \t". $freq{$sorted[$i]}/$count_words*100 . "\n";
   }
-  print "\#\#\#\#\#\#\#\#\#\# OUTPUT Test \#\#\#\#\#\#\#\#\#\#\n\n";
+  print "sur " . $count_words . " mots\n";
+  print "\#\#\#\#\#\#\#\#\#\#\#\#\#\n" ;  
+  return %hash ;
 
-  # here comes the nasty stuff :
-  # Hash of Hashes !
-  # (at first we wanted a variable with a variable name,
-  # but that's a bad thing to do according to the internet
-  $poids_langues{ $_[0] } = {%freq} ;
 }
 
-# files to compare with :
+sub calcul {
+    my %file = %{$_[0]} ;
+    my %french = %{$_[1]} ;
+    
+    my $total_french = 0 ;
+    # compare frequences
+    while(($key,$value) = each(%file)){
+	if (exists $french{$key}) {
+    	    my $result = abs ($value - $french{$key}) ;
+    	    $total_french += $result ;
+    	}
+    	else {
+    	    $total_french += $value ;
+    	}
+    }
+    	print $total_french ;
+}
 
-# open(DUTCH, '<:utf8', "corpus/?") ;
-# open(ENGLISH, '<:utf8', "corpus/?") ;
-# open(FRENCH, '<:utf8', "corpus/belle-rose.txt") ;
-# open(GERMAN, '<:utf8', "corpus/?") ;
-# open(ITALIAN, '<:utf8', "corpus/?") ;
+sub calcul2 {
+    my %file = %{$_[0]} ;
+    my %corpus = %{$_[1]} ;
 
-# &poids(DUTCH) ;
-# &poids(ENGLISH) ;
-# &poids(FRENCH) ;
-# &poids(GERMAN) ;
-# &poids(ITALIAN) ;
+    my $total_corpus = 0 ;
+    # compare frequences
+    while(($key,$value) = each(%file)){
+	if (exists $corpus{$key}) {
+    	    $total_corpus += $corpus{$key} ;
+    	}
+    }
+    	return $total_corpus ;
+}
 
-# close(DUTCH);
-# close(ENGLISH);
-# close(FRENCH);
-# close(GERMAN);
-# close(ITALIAN) ;
-
-
+sub test {
+    my %test = %{$_[0]} ;
+    while( my ($k, $v) = each(%test) ) {
+        print "key: $k, value: $v.\n";
+    }
+}
 
 ############
 # output ! #
 ############
 
 sub answer {
-  print "La langue du texte d'après l'analyse des mots est :\n"
+    my %results = (
+	french => $_[0],
+	english => $_[1]
+	);
+
+    my @sorted = sort { ( $results{$b} <=> $results{$a}) or ($a cmp $b) } keys %results ;
+
+    print "La langue du texte d'après l'analyse des mots est :\n";
+    print $sorted[0] . "\n";
+
 }
 
 
--- Programmation/Projet/projet.pl
+++ Programmation/Projet/projet.pl
@@ -17,12 +17,40 @@ use MethodeSuffixes;
 # Allemand, Anglais, Français, Italien et Néerlandais
 
 print "Bonjour, quel est le nom du fichier à analyser ?\n" ;
-chomp (my $file = <STDIN>) ;
+chomp (my $file = "README" ) ; #<STDIN>) ;
 open(FILE, '<:utf8', $file) ;
 
-MethodeMots::poids("FRENCH", "corpus/belle-rose.txt");
-MethodeMots::poids("FILE", $file);
-MethodeMots::answer;
+# var depending upon languages :
+my (%file, %french, %english) ;
+my @textes = (
+    {lang => "?", name => \%file, fh => "FILE", corpus => $file },
+    {lang => "french", name => \%french, fh => "FRENCH", corpus => "corpus/belle-rose.txt" },
+    {lang => "english", name => \%english, fh => "ENGLISH", corpus => "corpus/gottfried-en.txt.txt" }
+    );
+
+
+# my $textes_nbr = scalar(@textes);
+# for (my $i=0 ; $i <$textes_nbr ; $i++)
+# {
+#     print "$textes[$i]{name} \t $textes[$i]{fh} \t $textes[$i]{corpus}\n";
+#     $textes[$i]{name} = MethodeMots::poids("$textes[$i]{fh}", $textes[$i]{corpus});
+# }
+
+# for (my $i=0 ; $i <$textes_nbr ; $i++)
+# {
+#     $total_$textes[$i]{name} = MethodeMots::calcul2(\%file, $textes[$i]{name});
+# }
+
+
+
+
+%french = MethodeMots::poids("FRENCH", "corpus/belle-rose.txt");
+%english = MethodeMots::poids("ENGLISH", "corpus/gottfried-en.txt");
+%file = MethodeMots::poids("FILE", $file);
+my $total_french = MethodeMots::calcul2(\%file, \%french);
+my $total_english = MethodeMots::calcul2(\%file, \%english);
+
+MethodeMots::answer($total_french, $total_english );
 MethodeSuffixes::main;
 
 print "Combinaison des résultats...\n"; # Is it supposed to take that long ?

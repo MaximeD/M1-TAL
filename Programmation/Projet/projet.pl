a79df7b00ad44c085b1b4c3ead9e94d0b84b84e2
MaximeD <akbarova.armia@gmail.com>
2011-11-24 15:31:15 +0100

    work in class

--- Programmation/Projet/projet.pl
+++ Programmation/Projet/projet.pl
@@ -5,7 +5,7 @@ use utf8;
 binmode(STDOUT, ":utf8");
 
 use MethodeMots;
-use MethodeSuffixes;
+#use MethodeSuffixes;
 
 ################
 # Core program #
@@ -27,23 +27,28 @@ my ($total_french, $total_english) ;
 my @textes = (
     {                         hash => \%file,    fh => "FILE",    corpus => $file },
     {total => $total_french,  hash => \%french,  fh => "FRENCH",  corpus => "corpus/belle-rose.txt" },
-    {total => $total_english, hash => \%english, fh => "ENGLISH", corpus => "corpus/gottfried-en.txt" }
+    {total => $total_english, hash => \%english, fh => "ENGLISH", corpus => "corpus/en.txt" }
     );
 
 
 my $textes_nbr = scalar(@textes);
 for (my $i=0 ; $i <$textes_nbr ; $i++)
 {
-    print "$textes[$i]{hash} \t $textes[$i]{fh} \t $textes[$i]{corpus}\n";
-    $textes[$i]{hash} = MethodeMots::poids("$textes[$i]{fh}", $textes[$i]{corpus});
+  if (exists $textes[$i]{total}) { 
+   print "$textes[$i]{hash}  \t $textes[$i]{fh} \t $textes[$i]{corpus}\n";
+    %{$textes[$i]{hash}} = MethodeMots::poids($textes[$i]{fh}, $textes[$i]{corpus})
+  };
 }
 
 for (my $i=0 ; $i <$textes_nbr ; $i++)
 {
   if (exists $textes[$i]{total}) {                                       # there is no total for $file <STDIN>
-    $textes[$i]{total} = MethodeMots::calcul2(\%file, $textes[$i]{hash});
+    $textes[$i]{total} = MethodeMots::calcul($textes[0]{corpus}, $textes[$i]{hash});
   }
 }
+
+
+MethodeMots::answer($textes[1]{total},$textes[2]{total});
 
 
 
@@ -53,8 +58,8 @@ for (my $i=0 ; $i <$textes_nbr ; $i++)
 #my $total_french = MethodeMots::calcul2(\%file, \%french);
 #my $total_english = MethodeMots::calcul2(\%file, \%english);
 
-MethodeMots::answer($total_french, $total_english );
-MethodeSuffixes::main;
+#MethodeMots::answer($total_french, $total_english );
+#MethodeSuffixes::main;
 
 print "Combinaison des résultats...\n"; # Is it supposed to take that long ?
 

3fe91f72dbd7b8311272a8d18196f08e79bebe50
Maxime Demolin <akbarova.armia@gmail.com>
2012-03-14 16:32:51 +0100

    first commit

---
+++ S2/Programmation/TD5/normalization.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+use Unicode::Normalize; 
+use Encode;
+binmode(STDIN,  'utf8');
+binmode(STDOUT, 'utf8');
+binmode(STDERR, 'utf8');
+
+ 
+=pod
+
+=head1 NAME
+
+B<Normalization>
+
+Remove diacritics
+
+=head1 Synopsis
+
+&normalization($file)
+
+=cut
+
+local $/ = undef;
+
+my $file   = "mtfr.tag" ;
+my $output = "mtfr.tag.out" ;
+
+&normalization($file);
+
+sub normalization {
+   my ($input) = @_ ;
+
+   open(INPUT, '<', $input) or die;
+   open(OUTPUT, '>', $output) or die;
+
+   my $string = <INPUT>;
+
+   $string = decode("iso-8859-1", $string);
+   $string = NFD($string);
+   $string =~ s/\pM//og;
+   print OUTPUT "$string\n";
+}
+
